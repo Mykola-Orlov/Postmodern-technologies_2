6db08dd3822afd2836a84e2880dfc67357563acd
Mykola <m.orlov2201@gmail.com>
2024-04-19 15:44:58 +0200

    lab_1 update, + test

--- лаба1/lab_1.py
+++ лаба1/lab_1.py
@@ -1,60 +1,10 @@
 import sys
-import unittest
-from io import StringIO
-
-# функція приймає рядок і виводить його оберненим, + якщо програма виконалася успішно повертається "0", якщо помилка то "1"
-def reverse_string(input_str):
+# функція приймає рядок і виводить його оберненим
+def reverse_string(actual_argument):
     try:
-        reversed_str = input_str[::-1]
-        print(reversed_str)
-        return 0
+        reversed_str = actual_argument[::-1]
+        sys.stdout.write(reversed_str)
+        sys.exit(0)
     except Exception as e:
         sys.stderr.write(f"Error: {e}\n")
-        return 1
-    
-
-class TestReverseString(unittest.TestCase):
-    # метод де створюємо об'єкти StringIO для перенаправлення stdout та stderr
-    def setUp(self):
-        self.stdout = StringIO()  # створюємо об'єкт StringIO для перенаправлення stdout
-        self.stderr = StringIO()  #          - || -                               stderr
-        sys.stdout = self.stdout  # перенаправляємо stdout на об'єкт StringIO
-        sys.stderr = self.stderr  #    - || -       stderr    - || -
-
-    # метод де повертаємо stdout та stderr до їх оригінальних станів
-    def tearDown(self):
-        sys.stdout = sys.__stdout__  # повертаємо stdout до його оригінального стану
-        sys.stderr = sys.__stderr__  # повертаємо stderr     - || -
-    
-    # метод де перевіряємо, чи виведений текст у stdout відповідає результату
-    def test_reverse_string_stdout(self):
-        input_data = "hello"
-        expected_output = "olleh"
-        reverse_string(input_data)
-        self.assertEqual(self.stdout.getvalue().strip(), expected_output) # отримуємо весь вміст, який був направлений у stdout, + видаляємо пробіли, + зрівнюємо результат який вийшов з очікуваним
-    
-    # метод де перевіряємо, чи виведений текст у stderr не є порожнім рядком
-    def test_reverse_string_stderr(self):
-        input_data = 123
-        reverse_string(input_data)
-        self.assertNotEqual(self.stderr.getvalue().strip(), "")
-    
-    # метод де перевіряємо, чи повертається коректний код виходу у випадку успішного виконання функції
-    def test_reverse_string_success_exit_code(self):
-        input_data = "hello"
-        exit_code = reverse_string(input_data)
-        self.assertEqual(exit_code, 0)
-    
-    # метод де перевіряємо, чи повертається коректний код виходу у випадку помилки під час виконання функції
-    def test_reverse_string_error_exit_code(self):
-        input_data = 123
-        exit_code = reverse_string(input_data)
-        self.assertEqual(exit_code, 1)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) == 1:  # якщо аргументів командного рядка немає
-        input_data = sys.stdin.readline().strip()  # зчитуємо вхідні дані з консолі
-        reverse_string(input_data)  # викликаємо функцію обробки рядка
-    else:
-        unittest.main()  # запускаємо тести
+        sys.exit(1)
---
+++ лаба1/test_lab_1.py
@@ -0,0 +1,24 @@
+import unittest
+from lab_1 import reverse_string
+
+class TestReverseString(unittest.TestCase):
+
+    def test_check(self):
+        with self.assertRaises(SystemExit) as cm: 
+            reverse_string("hello") 
+            self.assertEqual(cm.exception.code, 0)
+
+        with self.assertRaises(SystemExit) as cm: 
+            reverse_string("123") 
+            self.assertEqual(cm.exception.code, 0)
+        
+        with self.assertRaises(SystemExit) as cm: 
+            reverse_string("0000") 
+            self.assertEqual(cm.exception.code, 0)
+        
+        with self.assertRaises(SystemExit) as cm: 
+            reverse_string("ggkg") 
+            self.assertEqual(cm.exception.code, 0)
+
+if __name__ == "__main__":
+    unittest.main()
