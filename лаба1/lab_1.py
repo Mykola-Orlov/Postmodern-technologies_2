ab049e4a2a4d2124db2137b9427e57d2a3049004
Mykola <m.orlov2201@gmail.com>
2024-04-19 11:19:39 +0200

    lab_1 update

--- лаба1/lab_1.py
+++ лаба1/lab_1.py
@@ -2,7 +2,7 @@ import sys
 import unittest
 from io import StringIO
 
-# функція приймає рядок і виводить його оберненим, + якщо праграма виконалася успішно повертається "0", якщо помилка то "1"
+# функція приймає рядок і виводить його оберненим, + якщо програма виконалася успішно повертається "0", якщо помилка то "1"
 def reverse_string(input_str):
     try:
         reversed_str = input_str[::-1]
@@ -14,28 +14,47 @@ def reverse_string(input_str):
     
 
 class TestReverseString(unittest.TestCase):
-    def test_reverse_string(self):
-        input_data = "hello" # вхід
-        expected_output = "olleh" # очікуваний вивід
-        with StringIO() as stdout:  # перенаправляємо вивід на stdout
-            with StringIO() as stderr:  # перенаправляємо вивід на stderr
-                sys.stdout = stdout
-                sys.stderr = stderr
-                self.assertEqual(reverse_string(input_data), 0) # викликаємо функцію і очікуємо результат "0"(програма успішно виконалася)
-                self.assertEqual(stdout.getvalue().strip(), expected_output) # перевіряємо чи вивід співпадає з очікуваним результатом
-                self.assertEqual(stderr.getvalue(), "") # перевіряємо чи є stderr порожній
+    # метод де створюємо об'єкти StringIO для перенаправлення stdout та stderr
+    def setUp(self):
+        self.stdout = StringIO()  # створюємо об'єкт StringIO для перенаправлення stdout
+        self.stderr = StringIO()  #          - || -                               stderr
+        sys.stdout = self.stdout  # перенаправляємо stdout на об'єкт StringIO
+        sys.stderr = self.stderr  #    - || -       stderr    - || -
 
-    def test_reverse_string_error(self):
-        input_data = 123  # вхід (не рядок)
-        with StringIO() as stdout:
-            with StringIO() as stderr:
-                sys.stdout = stdout
-                sys.stderr = stderr
-                self.assertEqual(reverse_string(input_data), 1) # викликаємо функцію і очікуємо результат "1"(Error)
-                self.assertEqual(stdout.getvalue(), "")
-                self.assertNotEqual(stderr.getvalue(), "")  # перевіряємо чи є щось в stderr, а не порожній рядок
+    # метод де повертаємо stdout та stderr до їх оригінальних станів
+    def tearDown(self):
+        sys.stdout = sys.__stdout__  # повертаємо stdout до його оригінального стану
+        sys.stderr = sys.__stderr__  # повертаємо stderr     - || -
+    
+    # метод де перевіряємо, чи виведений текст у stdout відповідає результату
+    def test_reverse_string_stdout(self):
+        input_data = "hello"
+        expected_output = "olleh"
+        reverse_string(input_data)
+        self.assertEqual(self.stdout.getvalue().strip(), expected_output) # отримуємо весь вміст, який був направлений у stdout, + видаляємо пробіли, + зрівнюємо результат який вийшов з очікуваним
+    
+    # метод де перевіряємо, чи виведений текст у stderr не є порожнім рядком
+    def test_reverse_string_stderr(self):
+        input_data = 123
+        reverse_string(input_data)
+        self.assertNotEqual(self.stderr.getvalue().strip(), "")
+    
+    # метод де перевіряємо, чи повертається коректний код виходу у випадку успішного виконання функції
+    def test_reverse_string_success_exit_code(self):
+        input_data = "hello"
+        exit_code = reverse_string(input_data)
+        self.assertEqual(exit_code, 0)
+    
+    # метод де перевіряємо, чи повертається коректний код виходу у випадку помилки під час виконання функції
+    def test_reverse_string_error_exit_code(self):
+        input_data = 123
+        exit_code = reverse_string(input_data)
+        self.assertEqual(exit_code, 1)
 
 
 if __name__ == "__main__":
-    unittest.main()
-    
+    if len(sys.argv) == 1:  # якщо аргументів командного рядка немає
+        input_data = sys.stdin.readline().strip()  # зчитуємо вхідні дані з консолі
+        reverse_string(input_data)  # викликаємо функцію обробки рядка
+    else:
+        unittest.main()  # запускаємо тести
